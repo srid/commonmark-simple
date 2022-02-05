3f83cbcecd8a72bc3ce04cd39a3035f1f414961c
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2022-02-04 21:57:06 -0500

    Delete Main.hs

--- src/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Main.Utf8 (withUtf8)
-
-main :: IO ()
-main = do
-  -- For withUtf8, see https://serokell.io/blog/haskell-with-utf8
-  withUtf8 $ do
-    putStrLn "Hello 🌎"
